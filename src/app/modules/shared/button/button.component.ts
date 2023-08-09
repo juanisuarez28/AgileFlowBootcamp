19aaa669a6c2a0c85e5a19f410fb610073bf26c0
FacuMozo <facumozo98@gmail.com>
2023-08-09 18:11:39 -0300

    tg-24 crear componente Button

--- src/app/modules/shared/button/button.component.ts
+++ src/app/modules/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
+
+  @Output() buttonEvent = new EventEmitter<boolean>();
+
+  @Input() icon = "";
+  @Input() text = "";
+  
+  displayText = false;
+  displayIcon = false;
+
+  assetsUrl = "../../../assets/";
+
+  buttonAction(){
+    this.buttonEvent.emit(true);
+  }
+  ngOnInit(): void {
+    if (this.text != '' ){
+      this.displayText = true;
+    }
+    if (this.icon != ""){
+      this.displayIcon = true;
+    }
+  }
+
 
 }
