739903bc96ea27ce45cfaa72753e43ea18d4580d
FacuMozo <facumozo98@gmail.com>
2023-08-13 23:41:04 -0300

    tg-26 crear componente generic card

--- src/app/modules/shared/generic-card/generic-card.component.ts
+++ src/app/modules/shared/generic-card/generic-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-generic-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./generic-card.component.scss']
 })
 export class GenericCardComponent {
+  @Input() title='Titulo:...';
+  @Input() description='Descripcion: ....';
 
 }
