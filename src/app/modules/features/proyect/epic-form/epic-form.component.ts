5a253e99dc395c85a9d5076365b9a8df117fa4d2
FacuMozo <facumozo98@gmail.com>
2023-08-16 18:29:43 -0300

    tg-39 y tg-40 Crear formulario pop-up para agregar epicas Crear formulario pop-up para edicion de epicas

---
+++ src/app/modules/features/proyect/epic-form/epic-form.component.ts
@@ -0,0 +1,54 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ProyectComponent } from '../proyect.component';
+import { FormBuilder, FormControl, FormGroup,Validators } from '@angular/forms';
+import { ProjectService } from '../../../core/services/project.service';
+
+@Component({
+  selector: 'app-epic-form',
+  templateUrl: './epic-form.component.html',
+  styleUrls: ['./epic-form.component.scss']
+})
+export class EpicFormComponent {
+
+  epicForm! : FormGroup;
+
+  constructor(
+    public dialogRef: MatDialogRef<ProyectComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: epicaForm,
+    private formBuilder : FormBuilder,
+    private projectService: ProjectService
+
+  ) { }
+
+
+
+  ngOnInit(): void {
+    this.epicForm = this.formBuilder.group({
+      project : new FormControl(this.data.project),
+      name : new FormControl(this.data.name,Validators.required),
+      description : new FormControl(this.data.description, Validators.required),
+      icon : new FormControl(this.data.icon, Validators.required)
+    })
+  }
+
+  onSubmit(){
+    if(this.epicForm.valid){
+      // console.log("Form is valid, form:", this.epicForm," close dialog");
+      this.dialogRef.close(this.epicForm);
+    }else{
+      
+      console.log("this form is not valid");
+    }
+  }
+
+
+}
+
+export interface epicaForm {
+  project: string;
+  name: string;
+  description: string;
+  icon: string;
+  option: string
+}
