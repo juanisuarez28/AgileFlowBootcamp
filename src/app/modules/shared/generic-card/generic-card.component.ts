f7f0cf8f2621f556de2e937359206872e619e4c8
FacuMozo <facumozo98@gmail.com>
2023-08-17 18:39:42 -0300

    tg-26 crear componente generic card

--- src/app/modules/shared/generic-card/generic-card.component.ts
+++ src/app/modules/shared/generic-card/generic-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-generic-card',
@@ -9,4 +9,19 @@ export class GenericCardComponent {
   @Input() title='Titulo:...';
   @Input() description='Descripcion: ....';
 
+  @Output() editOutPut = new EventEmitter();
+  @Output() deleteOutPut = new EventEmitter();
+
+  editEvent(){
+    console.log("editEvent");
+    
+    this.editOutPut.emit()
+  }
+  
+  deleteEvent(){
+    console.log("deleteEvent");
+    
+    this.deleteOutPut.emit()
+
+  }
 }
