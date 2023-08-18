82d8ff001330eb905b6f51b481c09dd1ed060704
juanisuarez38 <juanisua3@gmail.com>
2023-08-18 17:06:15 -0300

    tg-58 y tg-60 metodos agregar y delete projectService

--- src/app/modules/core/services/projects/project.service.ts
+++ src/app/modules/core/services/projects/project.service.ts
@@ -6,12 +6,21 @@ import { Project } from 'src/app/modules/models/cproject.model';
   providedIn: 'root'
 })
 export class ProjectService {
+ 
   private projects : Project[]= [];
   private projects$ = new Subject<Project[]>();
 
   constructor() {
 
-   }
+  }
+
+  newProject(value: any) {
+    let newId=this.projects[this.projects.length-1].getId()+1;
+    let aux= new Project(value.members, newId, value.name, value.description, "x", "messi", 2);
+
+    this.projects.push(aux);
+    this.projects$.next(this.projects);
+  }
 
   public getProjects$(): Observable<Project[]> {
     return this.projects$.asObservable();
@@ -20,6 +29,13 @@ export class ProjectService {
   public addProject(project: Project) {
     this.projects.push(project);
     this.projects$.next(this.projects); // emitimos el nuevo valor
+  }
+
+  public deleteProject(id:number){
+
+    this.projects=this.projects.filter(project => project.getId()!==id);
+    console.log("Proyecto con id ", id, " eliminado");
+    this.projects$.next(this.projects);
   }
 
 }
