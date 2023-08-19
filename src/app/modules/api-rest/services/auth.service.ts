467d7deb967988a8320bf3eb138d42e7a9c109fd
juanisuarez38 <juanisua3@gmail.com>
2023-08-19 19:30:03 -0300

    tg-63 crear servicio de autorizacion

--- src/app/modules/api-rest/services/auth.service.ts
+++ src/app/modules/api-rest/services/auth.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  baseUrl: string = " https://lamansysfaketaskmanagerapi.onrender.com/api";
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  postLogin(user : any): Observable<any>{
+    return this.http.post(this.baseUrl+'/login', user);
+  }
+
+}
+
+export interface User{
+  username: string;
+  password: string;
 }
