4ddba10a3df6c8592dc4a326d3871cf978863f9c
juanisuarez38 <juanisua3@gmail.com>
2023-08-23 01:17:42 -0300

    fix select para elegir a quien esta asignada una historia

--- src/app/modules/features/story-form/story-form.component.ts
+++ src/app/modules/features/story-form/story-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import { EpicComponent } from '../epic/epic.component';
+import { User } from '../../models/user.model';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-story-form',
@@ -11,28 +13,31 @@ import { EpicComponent } from '../epic/epic.component';
 export class StoryFormComponent implements OnInit{
 
   storyForm !: FormGroup;
+  users?:User[];
 
-  constructor(private fb: FormBuilder, public dialogRef : MatDialogRef<EpicComponent>,
+  constructor(private fb: FormBuilder, public dialogRef : MatDialogRef<EpicComponent>, private userService : UserService,
      @Inject(MAT_DIALOG_DATA) 
-     public data : {epic : string, name : string,
-      description : string, points : number, assignedTo: [],
-      created : Date, due :Date, start :Date,
-      end : Date, status : string, option : string}){}
+     public data : storyForms){}
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe( resp => {
+      this.users = resp;
+    });
 
     this.storyForm = this.fb.group({
       epic : new FormControl(this.data.epic),
       name: new FormControl(this.data.name, Validators.required),
       description: new FormControl(this.data.description, Validators.required),
       points: new FormControl(this.data.points, Validators.required),
-      assignedTo: new FormControl(this.data.assignedTo),
+      assignedTo: new FormControl(this.data.assignedTo, Validators.required),
       created : new FormControl(this.data.created),
       due: new FormControl(this.data.due),
       started: new FormControl(this.data.start),
       finished: new FormControl(this.data.end),
       status: new FormControl(this.data.status),
-    })
+    });
+
+   
   }
 
   onSubmit(){
@@ -45,3 +50,16 @@ export class StoryFormComponent implements OnInit{
   }
 }
 
+export interface storyForms{
+  epic : string;
+  name : string;
+  description : string;
+  points : number;
+  assignedTo: string[];
+  created : Date; 
+  due :Date;
+  start :Date;
+  end : Date;
+  status : String; 
+  option : string;
+}
